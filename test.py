121682b39a396b7b4686937633d48728e8bd2b92
DodoWhy <tapicationer@gmail.com>
2023-08-20 19:20:38 +0700

    cv.screenshot_window

--- test.py
+++ test.py
@@ -6,3 +6,7 @@ player = RobloxAgent()
 # Recording the screen
 while True:
     player.get_observation()
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+print('Done.')
