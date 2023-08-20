3c7c41a7cb8cbcfe8d99cfa09a458395e54f552e
DodoWhy <tapicationer@gmail.com>
2023-08-20 17:46:42 +0700

    TODO: RobloxAgent.get_observation()

--- roblox_agent.py
+++ roblox_agent.py
@@ -4,3 +4,6 @@ class RobloxAgent():
     def send_control(self, walk_forward: bool):
         pydirectinput.keyDown('w') if walk_forward else pydirectinput.keyUp('w')
         pass
+    def get_observation():
+        # TODO!
+        pass
